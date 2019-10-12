61b8488f3592b7a877ae70787ec3542cd29fb8f9
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-11 22:06:34 -0700

    Add function to check all checkboxes inbetween with shftkey.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-const handleCheck = e => {
-    console.log(e);
-}
 
 (function(){
     const checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+    let lastChecked;
+    const handleCheck = (e) => {
+        // check if they had the shift key down
+        let inBetween = false;
+        if(e.shiftKey && e.currentTarget.checked) {
+            // loop through every checkbox
+            checkboxes.forEach(checkbox => {
+                console.log(checkbox);
+                if (checkbox === e.currentTarget || checkbox === lastChecked) {
+                    inBetween = !inBetween;
+                    console.log('check in between');
+                }
+                if (inBetween) {
+                    checkbox.checked = true;
+                }
+            });
+        }
+        lastChecked = e.currentTarget;
+    };
+
     checkboxes.forEach(checkbox => {
         checkbox.addEventListener('click', handleCheck);
     });
